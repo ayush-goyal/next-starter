63bb0fbffde8833cda62fa15d456c4eaa82db9e2
Ayush Goyal <agoyal2001@gmail.com>
2025-03-22 15:10:37 -0400

    Disable sentry in local dev

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,12 +6,14 @@
 import * as Sentry from "@sentry/nextjs";
 import { env } from "@/env";
 
-Sentry.init({
-  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+if (process.env.NODE_ENV === "production" && env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({
+    dsn: env.NEXT_PUBLIC_SENTRY_DSN,
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
