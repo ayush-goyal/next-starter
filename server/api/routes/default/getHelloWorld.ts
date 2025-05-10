796cc4cef64f724cc71bf3add82494271a7cedd7
Ayush Goyal <agoyal2001@gmail.com>
2025-05-10 19:52:33 -0400

    Update procedure format

--- server/api/routes/default/getHelloWorld.ts
+++ server/api/routes/default/getHelloWorld.ts
@@ -1,19 +1,12 @@
-import { type Context, publicProcedure } from "server/api/trpc";
+import { publicProcedure } from "server/api/trpc";
 import { z } from "zod";
 
 const GetHelloWorldInputSchema = z.object({
   name: z.string().optional().default("World"),
 });
 
-interface Options {
-  ctx: Context;
-  input: z.infer<typeof GetHelloWorldInputSchema>;
-}
-
-const getHelloWorld = async ({ input }: Options) => {
-  return `Hello ${input.name} from server`;
-};
-
 export default publicProcedure
   .input(GetHelloWorldInputSchema)
-  .query(getHelloWorld);
+  .query(async ({ input }) => {
+    return `Hello ${input.name} from server`;
+  });
